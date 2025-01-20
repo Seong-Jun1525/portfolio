619538474758641dccef409a84d97b0737d4616d
Seong-Jun1525 <sjsj123455@naver.com>
2025-01-20 09:56:42 +0900

    canvas height 수정

--- src/components/3DMenu/CanvasLayout.jsx
+++ src/components/3DMenu/CanvasLayout.jsx
@@ -28,8 +28,8 @@ const CanvasLayout = ({children}) => {
 };
 
 const Container = styled.div`
-    width: 100vw;
-    height: 75vh;
+    width: 100%;
+    height: 100vh;
     display: flex;
     justify-content: center;
 `;
